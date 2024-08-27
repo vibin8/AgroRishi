16bd3f2ed1d6e20c843fd395ba3d1f3e0c23584d
Priya Kumari <59165049+vibin8@users.noreply.github.com>
2024-08-27 17:38:11 +0530

    Update __init__.py

--- agrorishi/__init__.py
+++ agrorishi/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e3cb708f2c7c257ae5b6b3ec4fe60e86'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:oraora@localhost:5432/agro"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/agro"
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
